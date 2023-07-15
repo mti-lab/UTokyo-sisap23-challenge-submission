245317ea0cfec903d97c8840b387aea4e169dfa8
maronuu <oguri.ytr.2000@gmail.com>
2023-07-15 10:03:05 +0000

    update ci

--- run_300K.sh
+++ run_300K.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-python search/search.py --algo PCA600,NSG32,Flat --size 300K -k 10 --threads 64 --ep kmeans --n-ep 58 --alpha 0.93619 --search-l 29 --outdir result
+python search/search.py --algo PCA600,NSG32,Flat --size 300K -k 10 --threads 64 --ep kmeans --n-ep 58 --alpha 0.93619 --search-l 26 --outdir result
